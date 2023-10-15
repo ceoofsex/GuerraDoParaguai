668969d58aaa03a2eab0ba3e23ed41e916795c9f
Setserp975 <127967205+Setserp975@users.noreply.github.com>
2023-10-14 22:45:34 -0300

    menu (animações, implementações e afins)
    ---

---
+++ GuerraDoParaguai/Assets/03_Pastas_Tomas/01_Imported_Things/menu_controller.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu_controller : MonoBehaviour
+{
+    public GameObject Canva_menu_pincipal;
+    public GameObject Canva_credito;
+    [SerializeField] private string game_scene;
+
+    public void canva_para_troca_do_credito()
+    {
+        Canva_menu_pincipal.SetActive(false);
+        Canva_credito.SetActive(true);
+    }
+
+    public void canva_para_troca_do_menu()
+    {
+        Canva_menu_pincipal.SetActive(true);
+        Canva_credito.SetActive(false);
+    }
+    public void exit_game()
+    {
+        Application.Quit();
+        print("funfou");
+    }
+}
