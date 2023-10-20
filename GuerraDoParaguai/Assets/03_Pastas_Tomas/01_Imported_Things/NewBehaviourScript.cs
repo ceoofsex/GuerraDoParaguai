142f482cb62427107f21fe057ba97b54073399e1
Setserp975 <127967205+Setserp975@users.noreply.github.com>
2023-10-20 11:45:05 -0300

    .

---
+++ GuerraDoParaguai/Assets/03_Pastas_Tomas/01_Imported_Things/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TxtSlowCharacter : MonoBehaviour
+{
+    TMP_Text txt;
+    public float speed = 1;
+    string tmpString;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        txt = GetComponent<TMP_Text>();
+        tmpString = txt.text;
+    }
+    private void OnEnable()
+    {
+        StartCoroutine(FlowText());
+    }
+
+    IEnumerator FlowText()
+    {
+        int stringTurn = -1;
+        txt.enabled = true;
+        txt.text = "";
+        while (stringTurn < tmpString.Length)
+        {
+            stringTurn++;
+            if (stringTurn < tmpString.Length)
+                txt.text = txt.text + tmpString[stringTurn];
+            yield return new WaitForSeconds(speed / 10);
+        }
+        yield break;
+
+    }
+}
+//Aproveite
